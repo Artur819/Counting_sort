c502b3d43feea4b04ce0a4d18d3ea233ae932858
Artur819 <arturhovhannisyan819@gmail.com>
2022-06-08 02:04:19 +0400

    counting sort

---
+++ counting_sort_implement.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+void Counting_Sort_arr(int *array, int size);
+void Print_Sort_arr(int *array, int size );
+int main() {
+  int array[] = {9,124,2,0,13,20,4,4,6,1,0};
+  int n = sizeof(array) / sizeof(array[0]);
+  Counting_Sort_arr(array, n);
+  Print_Sort_arr( array, n);
+}
+void Counting_Sort_arr(int *array, int size) {
+  int output[size+1];
+  int max_elem = array[0];
+  int min_elem = array[0];
+  for (int i = 1; i < size; ++i) {
+    if (array[i] > max_elem)
+      max_elem = array[i];
+    if (array[i]<min_elem){
+      min_elem = array[i];  
+    }    
+  }
+  int new_arr[max_elem-min_elem+1]={};
+  for (int i = 0; i < size; ++i) {
+    new_arr[array[i]]=new_arr[array[i]]+1;
+  }
+  for (int i = 1; i <= max_elem; ++i) {
+    new_arr[i] += new_arr[i - 1]; 
+  }
+  for (int i = size - 1; i >= 0; --i) {   
+    output[new_arr[array[i]] - 1] = array[i];
+    new_arr[array[i]]--;
+  }
+  for (int i = 0; i < size; ++i) {
+    array[i] = output[i];
+  }
+}
+void Print_Sort_arr(int *array, int size ) {
+  std::cout << "Sorted array: ";
+  for (int i = 0; i < size; ++i){
+    std::cout << array[i] << " " ; 
+  }
+  std::cout <<std::endl;
+}
---
+++ counting_sort_vector_implement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+void Sort(std::vector<int>& arr);
+void print(std::vector<int>& arr); 
+
+int main()
+{
+   std::vector<int> arr = { -5, -10, 0, -3, 8, 5, -1, 10 };
+    Sort(arr);
+    print(arr);
+    return 0;
+}
+
+void sort(std::vector<int>& arr)
+{
+    int max = *max_element(arr.begin(), arr.end());
+    int min = *min_element(arr.begin(), arr.end());
+    
+    int range = max - min + 1;
+    std::vector<int> count(range);
+    std::vector<int> output(arr.size());
+    for (int i = 0; i < arr.size(); i++){
+        count[arr[i] - min]++;
+    }
+    for (int i = 1; i < count.size(); i++){
+        count[i] += count[i - 1];
+    }
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[count[arr[i] - min] - 1] = arr[i];
+        count[arr[i] - min]--;
+    }
+    for (int i = 0; i < arr.size(); i++){
+        arr[i] = output[i];
+    }
+}
+ 
+void printArray(std::vector<int>& arr)
+{
+    for (int i = 0; i < arr.size(); i++){
+       std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}

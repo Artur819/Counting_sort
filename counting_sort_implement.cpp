f5adee4260b9901ef6862f876cc5237362725bb5
Artur819 <arturhovhannisyan819@gmail.com>
2022-06-10 13:55:13 +0400

    add

--- counting_sort_implement.cpp
+++ counting_sort_implement.cpp
@@ -3,10 +3,10 @@
 void Counting_Sort_arr (int *array, int size);
 void Print_Sort_arr (int *array, int size);
 int main() {
-  int array[] = {136,9,124,2,0,13,20,4,4,6,1,0};
+  int array[] = { 136, 9, 124, 2, 0, 13, 20, 4, 4, 6, 1, 0};
   int n = sizeof(array) / sizeof(array[0]);
-  Counting_Sort_arr(array, n);
-  Print_Sort_arr( array, n);
+  Counting_Sort_arr (array, n);
+  Print_Sort_arr ( array, n);
 }
 void Counting_Sort_arr (int *array, int size) {
   int output[size + 1];
@@ -21,7 +21,7 @@ void Counting_Sort_arr (int *array, int size) {
   }
  int * new_arr = new int [max_elem-min_elem+1];
   for (int i = 0; i < size; ++i) {
-    new_arr[array[i]] = new_arr[array[i]]+1;
+    new_arr[array[i]] = new_arr[array[i]] + 1;
   }
   for (int i = 1; i <= max_elem; ++i) {
     new_arr[i] += new_arr[i - 1]; 
@@ -36,9 +36,9 @@ void Counting_Sort_arr (int *array, int size) {
   delete [] new_arr;
 }
 void Print_Sort_arr (int *array, int size) {
-  std::cout << "Sorted array: ";
+  std::cout << "Sorted array : ";
   for (int i = 0; i < size; ++i){
-    std::cout << array[i] << " " ; 
+    std::cout << array[i] << " "; 
   }
   std::cout << std::endl;
 }
